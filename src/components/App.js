4748cc8c4750a378c15af033454d45743042fe4b
bishalsapkota <bishalsap2012@gmail.com>
2019-02-20 17:21:40 +0545

    second Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,22 @@
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import SongList from "./SongList";
 import SongDetail from "./SongDetail";
 
 const App = () => {
   return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <SongList />
-        </div>
-        <div className="column eight wide">
-          <SongDetail />
+    <Router>
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+          <div className="column eight wide">
+            <SongDetail />
+          </div>
         </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
