92714f14e4da09b11ce518ad2bf19248899147f8
bishalsapkota <bishalsap2012@gmail.com>
2019-02-20 17:30:46 +0545

    Updates

---
+++ precache-manifest.0ce71db04d9361ec6c16d8da9220f70a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9d212bee389dc7a9ba1",
+    "url": "/songsDetail/static/js/main.b9d212be.chunk.js"
+  },
+  {
+    "revision": "5e17f0600c3dd26a0f1f",
+    "url": "/songsDetail/static/js/runtime~main.5e17f060.js"
+  },
+  {
+    "revision": "2d5344647eaf2573a858",
+    "url": "/songsDetail/static/js/2.2d534464.chunk.js"
+  },
+  {
+    "revision": "8595eb30309f15b2ff076450c8f2adf8",
+    "url": "/songsDetail/index.html"
+  }
+];
